8b85117e841b1d50b8a65d013daf6f93d5ad609a
Holger Schill <Holger.Schill@itemis.de>
2013-03-24 21:53:39 -0400

    Script to start server

---
+++ org.eclipse.xtext.tutorial.survey.example/src/main/StartServer.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.eclipse.xtext.tutorial.survey.runtime.impl.SurveyServer;
+
+public class StartServer {
+	
+	public static void main(final String... args) {
+		SurveyServer surveyServer = new SurveyServer();
+		surveyServer.setPort(8080);
+		surveyServer.setPageFlow(new PageFlow());
+		surveyServer.addWebroot("./src-gen");
+		surveyServer.addWebroot("./html-gen");
+		surveyServer.addWebroot("../org.eclipse.xtext.tutorial.survey.runtime/webroot");
+		surveyServer.start();
+	}
+}
